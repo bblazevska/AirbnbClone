1bf57396c2e4715805e0dd3f37647eff337ef86a
kuzeva <kuzeva01@gmail.com>
2021-08-12 15:23:43 +0200

    navigation popup added /doesn't appear

---
+++ main.js
@@ -0,0 +1,7 @@
+// var nav_btn = document.querySelector('navigation__button');
+// var nav_popup = document.querySelector('#nav-popup');
+
+// nav_btn.addEventListener('click', () =>{
+//    nav_popup.classList.toggle('popup-show');
+   
+// });
